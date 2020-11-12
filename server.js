b51ba417dfb600bb1ca4425b526404cb873e1d1e
Matt Pearce <pearce.matthewa@gmail.com>
2020-11-11 20:34:42 -0700

    update server.js for heroku

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const PORT = process.env.PORT || 8080;
 const db = require("./models");
 const app = express();
 
-app.use(express.static("./client/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+}
+
 app.use(
   session({
     secret: "epsteinDidntKillHimself",
